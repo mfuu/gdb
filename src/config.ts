84819d93e664f3fffceb481f772eb5acaff79774
mfuu <446310957@qq.com>
2025-08-20 10:57:07 +0800

    chore: date format

--- src/config.ts
+++ src/config.ts
@@ -19,6 +19,8 @@ export default {
   dir: 'ltr', // "rtl" | "auto"
   lang: 'en', // html lang code. Set this empty and default will be "en"
   timezone: 'Asia/Shanghai', // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  dateFormat: 'MMM D, YYYY',
+  dateDetailFormat: 'MMMM D, YYYY, h:mm A',
 
   socials: [
     {
